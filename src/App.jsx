521efe6cc4f0ee4e55cc83379d99aa36714cecd3
Draymondo <167997928+Draymondo@users.noreply.github.com>
2026-09-17 00:14:40 +0000

    fix: restore auth and theme providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import { ThemeProvider } from './context/ThemeContext'
 import { VaultProvider } from './context/VaultContext'
 import ExportLauncher from './components/ExportLauncher'
 import HomePage from './pages/HomePage'
@@ -10,16 +12,20 @@ import ExportPage from './pages/ExportPage'
 
 export default function App() {
   return (
-    <VaultProvider>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/note/:id" element={<NotePage />} />
-        <Route path="/vault" element={<VaultPage />} />
-        <Route path="/vault/note/:id" element={<VaultNotePage />} />
-        <Route path="/export" element={<ExportPage />} />
-      </Routes>
-      <ExportLauncher />
-    </VaultProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <VaultProvider>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/note/:id" element={<NotePage />} />
+            <Route path="/vault" element={<VaultPage />} />
+            <Route path="/vault/note/:id" element={<VaultNotePage />} />
+            <Route path="/export" element={<ExportPage />} />
+          </Routes>
+          <ExportLauncher />
+        </VaultProvider>
+      </ThemeProvider>
+    </AuthProvider>
   )
 }
