1e54be1f9e28ad814cfcccd74be7054ecdb69814
Draymondo <167997928+Draymondo@users.noreply.github.com>
2026-09-23 17:21:49 +0000

    fix: close vault note item markup

--- src/pages/VaultPage.jsx
+++ src/pages/VaultPage.jsx
@@ -241,6 +241,7 @@ export default function VaultPage() {
                   <X size={16} />
                 </button>
               </div>
+            </div>
             ))}
           </div>
         )}
